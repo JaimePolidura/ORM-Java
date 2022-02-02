e943d6135a31e9b4ef12781f0c64e67ae9354119
JaimeTruman <jaime.polidura@gmail.com>
2022-02-02 17:49:57 +0100

    add lazy connection to the db

--- src/main/java/es/jaime/configuration/DatabaseConfiguration.java
+++ src/main/java/es/jaime/configuration/DatabaseConfiguration.java
@@ -12,22 +12,17 @@ import java.util.List;
 public abstract class DatabaseConfiguration {
     @Getter private Connection connection;
 
-    @SneakyThrows
-    public final void connect(){
-        if(connection == null || connection.isClosed()){
-            Class.forName("com.mysql.jdbc.Driver");
-            this.connection = DriverManager.getConnection(url());
-        }
+    protected abstract String url();
+
+    public List<String> getCommandsToRun(){
+        return Collections.EMPTY_LIST;
     }
 
     @SneakyThrows
     public void runCommands(){
-        if(connection == null || connection.isClosed()){
-            this.connect();
-        }
-        if(getCommandsToRun().isEmpty()){
-            return;
-        }
+        connectIfNotConnected();
+
+        if(getCommandsToRun().isEmpty()) return;
 
         Statement statement = connection.createStatement();
         List<String> commandsToRun = this.getCommandsToRun();
@@ -37,25 +32,35 @@ public abstract class DatabaseConfiguration {
         }
     }
 
-    public List<String> getCommandsToRun(){
-        return Collections.EMPTY_LIST;
-    }
-
-    protected abstract String url();
-
     public final ResultSet sendQuery(ReadQuery query) throws SQLException {
+        connectIfNotConnected();
+
         return connection.createStatement().executeQuery(query.toString());
     }
 
     public final ResultSet sendQuery(String query) throws SQLException {
+        connectIfNotConnected();
+
         return connection.createStatement().executeQuery(query);
     }
 
     public final void sendUpdate(WriteQuery query) throws SQLException {
+        connectIfNotConnected();
+
         connection.createStatement().executeUpdate(query.toString());
     }
 
     public final void sendUpdate(String query) throws SQLException {
+        connectIfNotConnected();
+
         connection.createStatement().executeUpdate(query);
+    }
+
+    @SneakyThrows
+    private void connectIfNotConnected(){
+        if(this.connection == null || connection.isClosed()){
+            Class.forName("com.mysql.jdbc.Driver");
+            this.connection = DriverManager.getConnection(url());
+        }
     }
 }
