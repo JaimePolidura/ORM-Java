1e161b2529c4e7f061a79bc8a28bc09b1d834f2d
Jaime Polidura <jaime.polidura@gmail.com>
2023-06-14 19:58:24 +0200

    fixed bug on ConditionalClassMapping

---
+++ src/main/java/es/jaime/CheckedFunction.java
@@ -0,0 +1,14 @@
+package es.jaime;
+
+@FunctionalInterface
+public interface CheckedFunction<I, O> {
+    O apply(I input) throws Exception;
+
+    default O applyOrRethrow(I input, String errorMessage) {
+        try{
+            return apply(input);
+        }catch (Exception e) {
+            throw new RuntimeException(errorMessage);
+        }
+    }
+}
--- src/main/java/es/jaime/repository/ConditionalClassMapping.java
+++ src/main/java/es/jaime/repository/ConditionalClassMapping.java
@@ -1,19 +1,16 @@
 package es.jaime.repository;
 
+import es.jaime.CheckedFunction;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 
 import java.sql.ResultSet;
 import java.util.Map;
-import java.util.Objects;
-import java.util.function.Function;
 
-@Builder
 @AllArgsConstructor
 public final class ConditionalClassMapping<E, T> {
+    @Getter private final CheckedFunction<ResultSet, T> typeValueAccessor;
     @Getter private final Map<T, Class<? extends E>> entitiesTypeMapper;
-    @Getter private final Function<ResultSet, T> typeBuilder;
     @Getter private final Class<T> typeClass;
 
     public static ConditionalClassMapping empty() {
@@ -21,8 +18,37 @@ public final class ConditionalClassMapping<E, T> {
     }
 
     public Class<? extends E> getMappingClass(ResultSet resultSet) {
-        T type = typeBuilder.apply(resultSet);
+        T type = typeValueAccessor.applyOrRethrow(resultSet, "Type not found on class " + typeClass.getName());
 
-        return Objects.requireNonNull(entitiesTypeMapper.get(type), String.format("Type %s not registered", type));
+        return entitiesTypeMapper.get(type);
+    }
+
+    public static <E, T> ConditionalClassMappingBuilder<E, T> builder() {
+        return new ConditionalClassMappingBuilder<>();
+    }
+
+    public static class ConditionalClassMappingBuilder<E, T> {
+        @Getter private CheckedFunction<ResultSet, T> typeValueAccessor;
+        @Getter private Map<T, Class<? extends E>> entitiesTypeMapper;
+        @Getter private Class<T> typeClass;
+
+        public ConditionalClassMapping<E, T> build() {
+            return new ConditionalClassMapping(typeValueAccessor, entitiesTypeMapper, typeClass);
+        }
+
+        public ConditionalClassMappingBuilder<E, T> entitiesTypeMapper(Map<T, Class<? extends E>> entitiesTypeMapper) {
+            this.entitiesTypeMapper = entitiesTypeMapper;
+            return this;
+        }
+
+        public ConditionalClassMappingBuilder<E, T> typeClass(Class<T> typeClass) {
+            this.typeClass = typeClass;
+            return this;
+        }
+
+        public ConditionalClassMappingBuilder<E, T> typeValueAccessor(CheckedFunction<ResultSet, T> typeValueAccessor) {
+            this.typeValueAccessor = typeValueAccessor;
+            return this;
+        }
     }
 }
